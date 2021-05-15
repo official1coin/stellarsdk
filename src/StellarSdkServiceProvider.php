2d93c83e2ca404ee2ee8488bdf96a08efdc1432f
Aaron Shames <aaron@1pbx.io>
2021-05-15 15:34:09 -0400

    1.0.7

--- src/StellarSdkServiceProvider.php
+++ src/StellarSdkServiceProvider.php
@@ -13,15 +13,15 @@ class StellarSdkServiceProvider extends ServiceProvider
         $configPath = __DIR__ . '/../config/stellarsdk.php';
         $this->mergeConfigFrom($configPath, 'stellarsdk');
 
-        $this->app->singleton(StellarSdk::class, function ($app) {
-            if (config('stellar_network') == 'public') {
-                return Server::publicNet();
-            } elseif (config('stellar_network') == 'test') {
-                return Server::testNet();
-            } elseif (config('stellar_network') == 'custom') {
-                return Server::customNet(config('stellar_custom_network'), 'Public Global Stellar Network ; September 2015');
-            }
-        });
+        // $this->app->singleton(Server::class, function ($app) {
+        //     if (config('stellar_network') == 'public') {
+        //         return Server::publicNet();
+        //     } elseif (config('stellar_network') == 'test') {
+        //         return Server::testNet();
+        //     } elseif (config('stellar_network') == 'custom') {
+        //         return Server::customNet(config('stellar_custom_network'), 'Public Global Stellar Network ; September 2015');
+        //     }
+        // });
     }
 
 
