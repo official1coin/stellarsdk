f5d5b187fa245996c8f5290237af2dbc197cbf6c
Aaron Shames <aaron@1pbx.io>
2021-05-15 15:01:09 -0400

    laravel stuff

--- config/stellarsdk.php
+++ config/stellarsdk.php
@@ -1,8 +1,11 @@
 <?php
 
 return [
-
-    'testnet' => ENV('STELLAR_TESTNET', null),
-    'publicnet' => ENV('STELLAR_PUBLICNET', null),
+    // can be any of:
+    // public - main publicnet horizon server
+    // test - test network
+    // custom - must also set STELLAR_CUSTOM_URL via ENV or config
+    'stellar_network' => ENV('STELLAR_NETWORK', 'public'),
+    'stellar_custom_url' => ENV('STELLAR_CUSTOM_URL', null),
 
 ];
--- src/StellarSdkServiceProvider.php
+++ src/StellarSdkServiceProvider.php
@@ -4,12 +4,24 @@ namespace OneCoin\StellarSdk;
 
 use Illuminate\Support\ServiceProvider;
 
+
+
 class StellarSdkServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $configPath = __DIR__ . '/../config/stellarsdk.php';
         $this->mergeConfigFrom($configPath, 'stellarsdk');
+
+        $this->app->singleton(StellarSdk::class, function ($app) {
+            if (config('stellar_network') == 'public') {
+                return Server::publicNet();
+            } elseif (config('stellar_network') == 'test') {
+                return Server::testNet();
+            } elseif (config('stellar_network') == 'custom') {
+                return Server::customNet(config('stellar_custom_network'), 'Public Global Stellar Network ; September 2015');
+            }
+        });
     }
 
 
